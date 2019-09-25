0aefb4415eb2fb71fba5e73d62450f15707bd217
CodyPalin <43323493+CodyPalin@users.noreply.github.com>
2019-09-24 23:08:48 -0600

    item properties added in main, item list created

--- src/grocery/Item.java
+++ src/grocery/Item.java
@@ -6,6 +6,15 @@ import java.util.Set;
 public class Item {
 	public int ID;
 	public String name;
+	public String ConstraintString; //string that contains the literal constraint of the item.
 	public int weight;
 	public Set<Integer> constraints = new HashSet<Integer>();
+	public Item() {
+		
+	}
+	public String ToString(){
+		
+		return ("ID="+ID+"name="+name+"weight="+weight+"constraints="+constraints);
+		
+	}
 }
