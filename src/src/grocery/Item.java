81a52a89aed0ffaaa6c74b191714398ac66af615
CodyPalin <43323493+CodyPalin@users.noreply.github.com>
2019-09-24 23:02:45 -0600

    update, Items are created in main
    constraints need building

---
+++ src/src/grocery/Item.java
@@ -0,0 +1,20 @@
+package grocery;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Item {
+	public int ID;
+	public String name;
+	public String ConstraintString; //string that contains the literal constraint of the item.
+	public int weight;
+	public Set<Integer> constraints = new HashSet<Integer>();
+	public Item() {
+		
+	}
+	public String ToString(){
+		
+		return ("ID="+ID+"name="+name+"weight="+weight+"constraints="+constraints);
+		
+	}
+}
