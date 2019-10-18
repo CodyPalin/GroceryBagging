a5d627e8963f631f0b3dda107fbb0b24b297470a
CodyPalin <cody.r.palin@gmail.com>
2019-10-18 11:09:42 -0600

    branch mainv2 to experimental

--- Main.java
+++ Main.java
@@ -1,57 +1,54 @@
-package grocery;
+//package grocery;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.ListIterator;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.Stack;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.StringTokenizer;
-import java.util.Vector;
 
 
-public class Main {
+public class Main_v2 {
 
 	private static int bagsize = 0;
 	public static int numbags=-1;
 	public static int sizebags=-1;
 	private static int totalItemWeight = 0;
-	private static long start;
+//	private static long start;
 	private static Stack<SearchState> states;
 	private static ArrayList<Bag> bags;
+	private static boolean doItOnce =  true;
 	
+	// TODO: adjusts 'doItOnce' in order to print out all depth-first search solutions.
+	
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
+		if(args.length ==0) {
+			System.out.println("failure");
+			System.exit(1);
+		}
 		if(args.length ==1||args[1].equals("-depth")) {
-			//System.out.println("depth");
-		start = System.currentTimeMillis();
+//		start = System.currentTimeMillis();
 		ArrayList<Item> Items = new ArrayList<Item>();
 		HashMap<String, Integer> hmap = new HashMap<String, Integer>();
 		HashMap<Integer, String> rhmap = new HashMap<Integer, String>();
 		Set<Integer> allindexes = new HashSet<Integer>();
-		//System.out.print(args[0]+"\n");
 		File filename = new File(args[0]);
 		if(!filename.exists()) {
-			System.out.println("File not found.\n");
-			return;
+			System.out.println("failure");
+			System.exit(1);
 		}
-		Scanner sc;
+		Scanner sc=null;
 		try {
 			sc = new Scanner(filename);
 		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-			return;
+			System.out.println("failure");
+			System.exit(1);
 		}
 		numbags = Integer.parseInt(sc.next());
 		bagsize = Integer.parseInt(sc.next());
@@ -81,8 +78,8 @@ public class Main {
 			x++;
 			linesc.close();
 		}
-		if(totalItemWeight > bagsize*numbags)
-			fail();
+//		System.out.println("Items: " + Items);
+		if(totalItemWeight > bagsize*numbags) fail();
 		for(Item i : Items)
 		{
 			Scanner cscan = new Scanner(i.ConstraintString);
@@ -116,17 +113,19 @@ public class Main {
 						Items.get(n).constraints.add(i.ID);
 					}
 				}
+//				System.out.println("Items: " + Items);
 			}
 			cscan.close();
 		}
-		/*for(Item item : Items)
-		{
-			System.out.println(item.toString());
-		}*/
 
 		sc.close();
-
+		
+//		System.out.println("Items (Before Sort):");
+//		for(Item item : Items) System.out.println(item.toString());
 		Collections.sort(Items, new ItemSort());
+//		System.out.println("Items (After Sort):");
+//		for(Item item : Items) System.out.println(item.toString());
+		
 		states = new Stack<SearchState>();
 		bags = new ArrayList<Bag>();
 		for(int i = 0;i<numbags;i++)
@@ -135,11 +134,14 @@ public class Main {
 			b.max = bagsize;
 			bags.add(b);
 		}
+//		System.out.println("bags: " + bags);
 		int minBagsUsed = Integer.MAX_VALUE;
 		SearchState bestbags = new SearchState(bags, 0);
-		//for(int i2 = 0; i2<5; i2++) {
-			//initialize starting state
-			SearchState currentstate = new SearchState(bags, 0);
+		
+		//initializes starting state.
+		SearchState currentstate = new SearchState(bags, 0);
+//		System.out.println("currentstate: " + currentstate);
+		labelB:
 			while(!currentstate.IsGoalState(Items.size())) 
 			{
 				//load state from stack
@@ -150,7 +152,6 @@ public class Main {
 				ArrayList<SearchState> tempstates = new ArrayList<SearchState>();
 				for(Bag b : bags) 
 				{
-					
 					if(b.canAdd(cItem))
 					{
 						//add onto stack
@@ -160,75 +161,132 @@ public class Main {
 						tempstates.add(new SearchState(newbags, currentItem+1));
 					}
 				}
-				//randomizes stack
-				//tempstates = randomize(tempstates); shuffles states
+				
+				{
+					Stack<SearchState> toBeRemoved = new Stack<SearchState>();
+					
+					labelC:
+					for(int i=0; i<tempstates.size()-1; i++) {
+							SearchState temp_ = tempstates.get(i);
+							
+							labelD:
+							for(int j=i+1; j<tempstates.size(); j++) {
+								SearchState temp_target_ = tempstates.get(j);
+								
+								if(temp_.bags.size()!=temp_target_.bags.size()) continue labelD;
+								for(Bag b: temp_.bags) Collections.sort(b.items);
+								for(Bag b: temp_target_.bags) Collections.sort(b.items);
+								
+								ArrayList<Bag> temp_bags_ = new ArrayList<Bag>();
+								temp_bags_.addAll((ArrayList<Bag>) temp_.bags.clone());
+								ArrayList<Bag> temp_target_bags_ = new ArrayList<Bag>();
+								temp_target_bags_.addAll((ArrayList<Bag>) temp_target_.bags.clone());
+								
+								labelF:
+								for(int k=0; k<temp_bags_.size(); k++) {
+									Bag temp_bags_each_ = temp_bags_.get(k);
+									ListIterator<Bag> target_itr = temp_target_bags_.listIterator();
+									labelE:
+									while(target_itr.hasNext()) {
+										Bag target_itr_bag_each_ = target_itr.next();
+										if(temp_bags_each_.items.size()!=target_itr_bag_each_.items.size()) continue labelE;
+										int g;
+										for(g=0; g<temp_bags_each_.items.size(); g++) {
+											if(temp_bags_each_.items.size()==0 && target_itr_bag_each_.items.size()==0) {
+												temp_target_bags_.remove(target_itr_bag_each_); continue labelF;
+											}//if size==0.
+											if(target_itr_bag_each_.items.contains((Integer)temp_bags_each_.items.get(g)));
+											else continue labelE;
+										}//for g.
+										temp_target_bags_.remove(target_itr_bag_each_);
+										if(temp_target_bags_.size()!=0) continue labelF;
+										toBeRemoved.add(temp_target_); continue labelC;
+									}//while hasNext.
+									//if(temp_target_bags_.size()==0) continue labelC;
+									continue labelD;
+								}//for k.
+								
+							}//for j.
+							
+					}//for i.
+					
+					while(!toBeRemoved.empty()) {
+						tempstates.remove(toBeRemoved.pop());
+					}//while !empty.
+					
+				}
+				
 				for(SearchState s:tempstates) {
 					states.push(s);
 				}
 				if(!states.empty()) {
-				currentstate = states.pop();
-				//System.out.println(currentItem);
-				}
-				else {
-					fail();
-				}
-			}
-			//System.out.println(currentstate.bags);
-			System.out.println("success");
-			int numbags = 0;
-			for(Bag b: bags)
-			{
-				if(b.items.size() != 0) 
-					numbags++;
-			}
-			if(numbags < minBagsUsed) {
-				minBagsUsed = numbags;
-				bestbags = currentstate;
-			}
+					
+					do {
+						currentstate = states.pop();
+						if(currentstate.IsGoalState(Items.size())) {
+							bags = currentstate.bags;
+							
+							int numbags = 0;
+							for(Bag b: bags)
+							{
+								if(b.items.size() != 0) 
+									numbags++;
+							}
+							if(numbags < minBagsUsed) {
+								minBagsUsed = numbags;
+								bestbags = currentstate;
+							}
+							
+							Collections.sort(currentstate.bags, new BagSort());
+							
+							System.out.println("success");
+							for(Bag b: currentstate.bags) 
+							{
+								Collections.sort(b.items);
+								for(int i: b.items) {
+									System.out.print(rhmap.get(i)+"\t");
+								}
+								if(!b.items.isEmpty()) {
+									System.out.println();
+								}
+							}
+							if(doItOnce) {
+								System.exit(0);
+							}
+							
+						}// if IsGoalState.
+						else { continue labelB; }
+						
+					}while(!states.empty());
+					
+				}// if !states.empty.
 				
-			System.out.println("Bags used:"+ numbags);
-			for(Bag b: currentstate.bags) 
-			{
+				else { fail(); }
 				
-				for(int i: b.items) {
-					System.out.print(rhmap.get(i)+"\t");
-				}
-				if(!b.items.isEmpty())
-					System.out.println();
-			}
-		//}
-			System.out.println((double)(System.currentTimeMillis()-start)/1000 + " seconds");
-			System.out.println("Minimum bags used: "+minBagsUsed);
-			System.out.println("Best Solution:");
-			for(Bag b: bestbags.bags) 
-			{
-				
-				for(int i: b.items) {
-					System.out.print(rhmap.get(i)+"\t");
-				}
-				if(!b.items.isEmpty())
-					System.out.println();
-			}
-		}
-		
-	}
-
-	private static ArrayList<SearchState> randomize(ArrayList<SearchState> tempstates) {
-		ArrayList<SearchState> tempstates2 = new ArrayList<SearchState>();
-		int size = tempstates.size();
-		for(int i = 0; i<size; i++)
+			}// while !IsGoalState.
+			
+		// displays the best solution (i.e. one of the best solutions).
+//		System.out.println("bestbags: ");
+		for(Bag b: bestbags.bags) 
 		{
-			int x =(int) (Math.random()*tempstates.size());
-			tempstates2.add(tempstates.remove(x));
+//				for(int i: b.items) {
+//					System.out.print(rhmap.get(i)+"\t");
+//				}
+				if(!b.items.isEmpty()) {
+//					System.out.println();
+				}
 		}
-		return tempstates2;
-	}
+			
+	}//depth search.
+		
+}//main.
 
 	private static void fail() {
 		System.out.println("failure");
 		//System.out.println((double)(System.currentTimeMillis()-start)/1000 + " seconds");
-		System.exit(0);
+		System.exit(1);
 	}
+	
 	static class ItemSort implements Comparator<Item> 
 	{ 
 		//negative if item a is more constrained than item b
@@ -251,5 +309,19 @@ public class Main {
 	    			return 0;
 	    	}
 	    } 
-	} 
-}
+	}// ItemSort.
+	
+	static class BagSort implements Comparator<Bag> 
+	{ 
+		@Override
+	    public int compare(Bag a, Bag b) 
+	    { 
+	    	if(a.weight > b.weight)
+	    		return -1;
+	    	else if(a.weight < b.weight)
+	    		return 1;
+	    	else return 0;
+	    } 
+	}// BagSort.
+	
+}//Main_v2.
